f9f8ef22adef9ec67b166c0b1eaa915e47c12bdf
fyiernzy <fyiernzy@gmail.com>
2023-01-05 11:24:10 +0800

    Add functionality to Money class

--- Lab8/L8Q7.java
+++ Lab8/L8Q7.java
@@ -4,7 +4,7 @@ package com.fyiernzy.Lab8;
 
 public class L8Q7 {
 	public static void main(String[] args) {
-		System.out.println(new Money(123.47).getNotesAndCoints());
+		System.out.println(new Money(123.47).getNotesAndCoins());
 		System.out.println(Money.add(34.56, 45.89).getAmountAsCash());
 	}
 }
@@ -21,7 +21,7 @@ class Money {
 		this.valueCent = round((int) (amount * 100));
 	}
 	
-	public static int round(int value) {
+	public int round(int value) {
 		return ((value + 2) / 5 * 5);
 	}
 	
@@ -33,39 +33,80 @@ class Money {
 		return this.valueCash;
 	}
 	
-	public static Money substract(Money one, Money two) {
-		return new Money(one.getAmountAsCash() - two.getAmountAsCash());
+	/**
+	 * Takes two double values and return a Money object
+	 * @param value_one a double value representing cash value.
+	 * @param value_two a double value representing cash value.
+	 * @return a Money object
+	 */
+	public static Money subtract(double value_one, double value_two) {
+		return new Money(Math.abs(value_one - value_two));
 	}
 	
-	public static Money substract(double value_one, double value_two) {
-		return substract(new Money(value_one), new Money(value_two));
+	public static Money subtract(Money one, Money two) {
+		return Money.subtract(one.getAmountAsCash(), two.getAmountAsCash());
+	}
+	
+	public Money subtract(double value) {
+		return Money.subtract(this.getAmountAsCash(), value);
+	}
+	
+	public Money subtract(Money money) {
+		return Money.subtract(this, money);
+	}
+	
+	
+	// Class method add
+	public static Money add(double value_one, double value_two) {
+		return new Money(value_one + value_two);
 	}
 	
 	public static Money add(Money one, Money two) {
-		return new Money(one.getAmountAsCash() + two.getAmountAsCent());
+		return Money.add(one.getAmountAsCash(), two.getAmountAsCash());
 	}
 	
-	public static Money add(double value_one, double value_two) {
-		return add(new Money(value_one), new Money(value_two));
+	public Money add(double value) {
+		return Money.add(this.getAmountAsCash(), value);
 	}
+	
+	public Money add(Money money) {
+		return Money.add(this, money);
+	}
+	
 
-	public String getNotesAndCoints() {
+	public String getNotesAndCoins() {
+		return getNotesAndCoins(true, true);
+	}
+	
+	public String getNotes() {
+		return getNotesAndCoins(true, false);
+	}
+	
+	public String getCoins() {
+		return getNotesAndCoins(false, true);
+	}
+	
+	public String getNotesAndCoins(boolean enableNotes, boolean enableCoins) {
 		int tmp = valueCent;
+		
 		StringBuilder sb = new StringBuilder();
 		
 		for(int i = 0; i < NOTES.length; i++) {
-			counts[i] = tmp / (NOTES[i] * 100);
-			sb.append(counts[i]).append(" x RM ").append(NOTES[i]).append("\n");
+			if(enableNotes) {
+				counts[i] = tmp / (NOTES[i] * 100);
+				sb.append(counts[i]).append(" x RM ").append(NOTES[i]).append("\n");
+			}
 			tmp %= (NOTES[i] * 100);
 		}
 		
-		for(int i = 0; i < COINS.length; i++) {
-			counts[NOTES.length + i] = tmp / COINS[i];
-			sb.append(counts[NOTES.length + i]).append(" x ").append(COINS[i]).append(" cents\n");
-			tmp %= COINS[i];
+		if (enableCoins) {
+			for(int i = 0; i < COINS.length; i++) {
+				counts[NOTES.length + i] = tmp / COINS[i];
+				sb.append(counts[NOTES.length + i]).append(" x ").append(COINS[i]).append(" cents\n");
+				tmp %= COINS[i];
+			}
 		}
 		
 		return sb.toString();
 	}
-
 }
