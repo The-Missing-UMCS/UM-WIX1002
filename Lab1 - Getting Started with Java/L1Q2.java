0476a7e6acd208a7f0afccf6cc6f0d2e3752fedb
fyiernzy <fyiernzy@gmail.com>
2023-02-06 20:04:20 +0800

    🌱 Rename folder

---
+++ Lab1 - Getting Started with Java/L1Q2.java
@@ -0,0 +1,14 @@
+package Lab1;
+
+
+public class L1Q2 {
+	public static void main(String[] args) {
+		System.out.println("Name          : Ng Zhi Yang");
+		System.out.println("Matric Number : 22004833");
+		System.out.println("Address       : 4， Jalan Ceria 23, Taman Nusah Indah");
+		System.out.println("Email         : fyiernzy@gmail.com");
+		System.out.println("Contact       : 017-780 9298");
+		System.out.println("Instagram     : @nzhiyang");
+	}
+
+}
