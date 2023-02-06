4f471791f90a9fd883f838434e0e57ef13568aa7
fyiernzy <fyiernzy@gmail.com>
2023-02-06 20:01:49 +0800

    Upload Viva2

---
+++ Viva/Viva2/Cloud.java
@@ -0,0 +1,19 @@
+package Viva.Viva2;
+
+public class Cloud {
+	protected final String CLOUD_PACKAGE;
+	protected double totalCost;
+	
+	Cloud(String cloudPackage) {
+		this.CLOUD_PACKAGE = cloudPackage;
+	}
+	
+	public double getTotalCost() {
+		return this.totalCost;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Cloud Package : %s  Total Cost= %.2f", this.CLOUD_PACKAGE, this.getTotalCost());
+	}
+}
