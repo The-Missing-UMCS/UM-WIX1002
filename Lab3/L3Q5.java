aa3ede877bb609d97828cb5518410c776ffed8bb
fyiernzy <55268504+fyiernzy@users.noreply.github.com>
2023-10-25 19:42:43 +0800

    ✨ Update L3Q5.java

--- Lab3/L3Q5.java
+++ Lab3/L3Q5.java
@@ -3,36 +3,21 @@ package Lab3;
 import java.util.Scanner;
 public class L3Q5 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Crammer's rule:");
-		System.out.println("ax + by = e, cx + dy = f");
-		System.out.println("x = (ed-bf) / (ad-bc) y=(af-ec) / (ad-bc)");
-		System.out.println("");
-		
-		char[] ch = {'a','b','c','d','e','f'};
-		int[] values = new int[6];
-		
-		for(int i = 0; i < 6; i++) {
-			System.out.print("Enter the value of " + ch[i] + " : ");
-			values[i] = scanner.nextInt();
-		}
-		
-		scanner.close();
-		
-		int denominator = values[0] * values[3] - values[1] * values[2];
-		
-		if (denominator == 0) {
-			System.out.println("The equation has no solution");
-		} else {
-			int numeratorX = values[4] * values[3] - values[1] * values[5]; 
-			int numeratorY = values[0] * values[5] - values[4] * values[2];	
-			int x = numeratorX / denominator;
-			int y = numeratorY / denominator;
-			
-			System.out.printf("The value of x = %d\nThe value of y = %d", x, y);
-		}
-		
-		
+		Scanner sc = new Scanner(System.in);
+        	System.out.println("Crammer's rule:\n" + 
+                            	"ax + by = e, cx + dy = f\n" +
+                            	"x = (ed-bf) / (ad-bc) y=(af-ec) / (ad-bc)\n");
+        int[] val = new int[6];
+        for(int i = 0; i < 6; i++) {
+		System.out.printf("Enter the value of %c : ", i + 'a');
+		val[i] = sc.nextInt();
+        }
+        sc.close();
+        int a = val[0], b = val[1], c = val[2], d = val[3], e = val[4], f = val[5]；
+	int denom = a * d - b * c;
+        if(denom == 0)
+            System.out.println("no solution");
+        else 
+            System.out.printf("x = %d, y = %d\n", (e * d - b * f) / denom, (a * f - e * c) / denom);
 	}
 }
