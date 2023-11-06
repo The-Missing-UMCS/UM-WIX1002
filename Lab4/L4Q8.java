9e6c7c091197627e444801ba688c59336f37cc6e
fyiernzy <55268504+fyiernzy@users.noreply.github.com>
2023-11-06 23:28:54 +0800

    Update L4Q8.java

--- Lab4/L4Q8.java
+++ Lab4/L4Q8.java
@@ -4,21 +4,14 @@ public class L4Q8 {
 
     public static void main(String[] args) {
         int n = (int) (Math.random() * 100);
-        boolean isPrime = true;
         System.out.printf("First %d prime number%s:\n", n, (n > 1) ? "s" : "");
-
+        outer: 
         for(int val = 2, cnt = 0; cnt < n; val++) {
-            isPrime = true;
-            for (int j = 2; j < val; j++) {
-                if (val % j == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if(isPrime) {
-                System.out.printf("%3d%s%s", val, (cnt == n - 1) ? "" : ", ", ((cnt + 1) % 10 == 0) ? "\n" : "");
-                cnt++;
-            }
+            for (int j = 2; j < val; j++) 
+                if (val % j == 0) 
+                    continue outer;
+            System.out.printf("%3d%s%s", val, (cnt == n - 1) ? "" : ", ", ((cnt + 1) % 10 == 0) ? "\n" : "");
+            cnt++;
         }
     }
 }
