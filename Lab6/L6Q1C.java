7da9f8e3bbccdc778078338fd315cd58e8742f08
fyiernzy <fyiernzy@gmail.com>
2023-12-05 12:16:10 +0800

    ✨ Amend Lab6

---
+++ Lab6/L6Q1C.java
@@ -0,0 +1,16 @@
+package com.fyiernzy.Lab6;
+
+import java.util.stream.IntStream;
+
+public class L6Q1 {
+	public static void main(String[] args) {
+		IntStream.rangeClosed(1, 20).forEach(x -> System.out.print(triangular(x) + " "));		
+	}
+	
+	public static int triangular(int n) {
+		if (n <= 1)
+			return n;
+		else
+			return n + triangular(n-1);
+	}
+}
