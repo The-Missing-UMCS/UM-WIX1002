5ac5b71353936363b10ece3d59d71f4ba7d1ac3a
zhiyang.ng <zhiyang.ng@antgroup.com>
2024-10-22 19:10:56 +0800

    [feat] add new constant

--- umwix1002/src/main/java/com/umwix1002/solution/lab/constants/CommonConstant.java
+++ umwix1002/src/main/java/com/umwix1002/solution/lab/constants/CommonConstant.java
@@ -8,6 +8,7 @@ public class CommonConstant {
     public static final Integer ZERO = 0;
     public static final Integer ONE = 1;
     public static final Integer TWO = 2;
+    public static final Integer TEN = 10;
     
     public static final String LINE_BREAK = System.lineSeparator();
     public static final String BLANK = "";
