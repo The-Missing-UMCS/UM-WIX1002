a8e858e2701f4529c9fde54e15947b59017227e2
zhiyang.ng <zhiyang.ng@antgroup.com>
2024-12-03 11:06:31 +0800

    [feat] add FileUtil for low-level file handling

--- umwix1002/src/main/java/com/umwix1002/solution/lab/util/FileUtil.java
+++ umwix1002/src/main/java/com/umwix1002/solution/lab/util/FileUtil.java
@@ -1,4 +1,31 @@
-package com.umwix1002.solution.lab.lab10.l10q4.dal;
+package com.umwix1002.solution.lab.util;
 
-public class FileReader {
+import java.io.*;
+
+import static com.umwix1002.solution.lab.constants.CommonConstant.LINE_BREAK;
+
+public class FileUtil {
+    public static void writeFile(String filePath, String content) throws IOException {
+        try(PrintWriter writer = new PrintWriter(filePath)) {
+            writer.println(content);
+        }
+    }
+    
+    public static String readFile(String filePath) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                sb.append(line).append(LINE_BREAK);
+            }
+        }
+        return sb.toString();
+    }
+    public static String getExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+    
+    public static String getFileName(String filePath) {
+        return filePath.substring(filePath.lastIndexOf("/") + 1);
+    }
 }
