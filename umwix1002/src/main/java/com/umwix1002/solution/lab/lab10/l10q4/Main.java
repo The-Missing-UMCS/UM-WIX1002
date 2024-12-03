1374a3afabf3968f1fb4658005a5a8751d064112
zhiyang.ng <zhiyang.ng@antgroup.com>
2024-12-03 11:23:12 +0800

    refactor: moved the logging logic

--- umwix1002/src/main/java/com/umwix1002/solution/lab/lab10/l10q4/Main.java
+++ umwix1002/src/main/java/com/umwix1002/solution/lab/lab10/l10q4/Main.java
@@ -5,8 +5,9 @@ import com.umwix1002.solution.lab.lab10.l10q4.domain.Appointment;
 import com.umwix1002.solution.lab.lab10.l10q4.domain.AppointmentDto;
 import com.umwix1002.solution.lab.lab10.l10q4.service.AppointmentService;
 import com.umwix1002.solution.lab.lab10.l10q4.util.AppointmentConverter;
-import com.umwix1002.solution.lab.lab10.l10q4.util.ConsoleLogger;
+import com.umwix1002.solution.lab.lab10.l10q4.util.LoggerUtil;
 import com.umwix1002.solution.lab.util.FileUtil;
+import com.umwix1002.solution.lab.util.TableUtil;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -14,9 +15,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import static com.umwix1002.solution.lab.Properties.*;
-import static com.umwix1002.solution.lab.constants.CommonConstant.LINE_BREAK;
+import static com.umwix1002.solution.lab.constants.CommonConstant.*;
+import static com.umwix1002.solution.lab.constants.ConsoleConstants.GREEN;
+import static com.umwix1002.solution.lab.constants.ConsoleConstants.RED;
 import static com.umwix1002.solution.lab.lab10.config.Constants.APPOINTMENT_FILE_PATH;
-import static com.umwix1002.solution.lab.lab10.l10q4.util.ConsoleLogger.logAppointments;
 
 /**
  * @author Ng Zhi Yang
@@ -42,9 +44,39 @@ public class Main {
     }
 
     private static void logResult(AppointmentDto newAppointmentDto, boolean isSuccess) {
+        // args
         LocalDate localDate = newAppointmentDto.getStart().toLocalDate();
         LocalTime start = newAppointmentDto.getStart().toLocalTime();
         LocalTime end = newAppointmentDto.getEnd().toLocalTime();
-        ConsoleLogger.logResult(localDate, start, end, isSuccess);
+
+        // logging properties
+        final String pattern = " ==> %2d-%2d-%d -->  %2d:%2s ~ %2d:%2s  --> Appointment %s." + System.lineSeparator();
+        final String color = isSuccess ? GREEN : RED;
+        final String label = isSuccess ? "Success" : "Failed";
+
+        LoggerUtil.logWithColor(color, pattern,
+            localDate.getDayOfMonth(), localDate.getMonthValue(), localDate.getYear(),
+            start.getHour(), "00", end.getHour(), "00", label);
+    }
+
+    public static void logAppointments(List<Appointment> appointments) {
+        final int[] counter = {1};
+        TableUtil.printTableWithData(new String[] {"Appointment", "Date", "Time"}, 
+            appointments.stream().map(
+                appointment -> {
+                    final String dateFormat = "%02d-%02d-%d ";
+                    final String timeFormat = "%02d:%02d  ";
+                    String[] data = new String[3];
+                    data[0] = String.valueOf(counter[0]++);
+                    data[1] = String.format(dateFormat, 
+                        appointment.getStart().getDayOfMonth(), 
+                        appointment.getStart().getMonthValue(), 
+                        appointment.getStart().getYear());
+                    data[2] = String.format(timeFormat + " - " + timeFormat,
+                        appointment.getStart().getHour(), appointment.getStart().getMinute(),
+                        appointment.getEnd().getHour(), appointment.getEnd().getMinute());
+                    return data;
+                }
+            ).toList());
     }
 }
