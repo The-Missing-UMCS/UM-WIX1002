d165848572b79d6d5cd1d92418b9af90f87089a0
fyiernzy <fyiernzy@gmail.com>
2025-11-11 08:46:32 +0800

    refactor: refine solution for lab05

--- umwix1002-advanced/src/main/java/com/umwix1002/solution/lab/lab05/l5q2b.java
+++ umwix1002-advanced/src/main/java/com/umwix1002/solution/lab/lab05/l5q2b.java
@@ -1,18 +1,17 @@
 package com.umwix1002.solution.lab.lab05;
 
-import com.umwix1002.solution.lab.constants.CommonConstant;
-
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
 public class l5q2b {
+
     public static void main(String[] args) {
         Set<Integer> set = new HashSet<>();
         Random random = new Random();
         while (set.size() < 10) {
             set.add(random.nextInt(1, 21));
         }
-        set.forEach(value -> System.out.print(value + CommonConstant.SPACE));
+        set.forEach(value -> System.out.print(value + " "));
     }
 }
