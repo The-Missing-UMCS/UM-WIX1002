b6b7b1bcf56ee4c9d63eaacdae520da2c2bfa985
fyiernzy <fyiernzy@gmail.com>
2025-11-21 13:12:35 +0800

    build: add /lab/constants/ for constants

---
+++ umwix1002-solutions/src/main/java/com/umwix1002/solution/lab/constants/ConsoleConstants.java
@@ -0,0 +1,26 @@
+package com.umwix1002.solution.lab.constants;
+
+public class ConsoleConstants {
+    public static final String RESET = "\033[0m";
+
+    // Text colors
+    public static final String BLACK = "\033[30m";
+    public static final String RED = "\033[31m";
+    public static final String GREEN = "\033[32m";
+    public static final String YELLOW = "\033[33m";
+    public static final String BLUE = "\033[34m";
+    public static final String PURPLE = "\033[35m";
+    public static final String CYAN = "\033[36m";
+    public static final String WHITE = "\033[37m";
+
+    // Background colors
+    public static final String BLACK_BACKGROUND = "\033[40m";
+    public static final String RED_BACKGROUND = "\033[41m";
+    public static final String GREEN_BACKGROUND = "\033[42m";
+    public static final String YELLOW_BACKGROUND = "\033[43m";
+    public static final String BLUE_BACKGROUND = "\033[44m";
+    public static final String PURPLE_BACKGROUND = "\033[45m";
+    public static final String CYAN_BACKGROUND = "\033[46m";
+    public static final String WHITE_BACKGROUND = "\033[47m";
+
+}
