a99937f73ceca9d2b349d11062bafa9e6092bac1
fyiernzy <fyiernzy@gmail.com>
2025-11-21 13:13:50 +0800

    test: add test folder for testing

---
+++ umwix1002-solutions/src/test/java/lab6/l6q4Test.java
@@ -0,0 +1,15 @@
+package lab6;
+
+import com.umwix1002.solution.lab.lab06.l6q4;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class l6q4Test {
+    @ParameterizedTest
+    @CsvSource({"24,8,8", "36,16,4", "272,148,4"})
+    void testReverse(int a, int b, int expected) {
+        assertEquals(expected, l6q4.findGCD(a, b));
+    }
+}
