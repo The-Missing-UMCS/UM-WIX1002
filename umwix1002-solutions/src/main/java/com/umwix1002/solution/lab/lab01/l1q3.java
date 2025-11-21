4563251c91f64af714631f6dbae0f15af2cb2ffd
fyiernzy <fyiernzy@gmail.com>
2025-11-21 13:11:01 +0800

    lab: compile and enhance solutions for lab01

---
+++ umwix1002-solutions/src/main/java/com/umwix1002/solution/lab/lab01/l1q3.java
@@ -0,0 +1,17 @@
+package com.umwix1002.solution.lab.lab01;
+
+public class l1q3 {
+    public static void main(String[] args) {
+        System.out.println("""
+                    **********		    **    		  *  		    *
+                	*        *		 *      * 		 *** 		   * *
+                	*        *		*        *		*****		  *   *
+                	*        *		*        *		  *  		 *     *
+                	*        *		*        *		  *  		*       *
+                	*        *		*        *		  *  		 *     *
+                	*        *		*        *		  *  		  *   *
+                	*        *		 *      * 		  *  		   * *
+                	**********		    **    		  *  		    *
+                """);
+    }
+}
